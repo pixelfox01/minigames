bd05bf29ade8777b0802d04e5cf18bda4579d795
Imran Yafith <imranyafith@gmail.com>
2023-12-04 22:01:48 -0500

    Created basic tic-tac-toe board

--- app/(games)/tictactoe/page.tsx
+++ app/(games)/tictactoe/page.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
+import Board from "./board/board";
 
 export default function TicTacToe() {
   return (
     <div>
       <Link href="/">Back to home</Link>
       <h1>Tic-Tac-Toe</h1>
-      <p>Coming soon...</p>
+      <Board />
     </div>
   );
 }
