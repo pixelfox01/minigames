79f8673fc3d4d9aceb86a6f1c2d9c014d568a4f3
Imran Yafith <imranyafith@gmail.com>
2023-12-04 21:22:48 -0500

    Created basic skeleton

---
+++ app/(games)/tictactoe/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function TicTacToe() {
+  return (
+    <div>
+      <Link href="/">Back to home</Link>
+      <h1>Tic-Tac-Toe</h1>
+      <p>Coming soon...</p>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <h1>Minigames</h1>
+      <div>
+        <Link href="/tictactoe">Tic-Tac-Toe</Link>
+        <p>Other games coming soon...</p>
+      </div>
+    </div>
+  );
 }
